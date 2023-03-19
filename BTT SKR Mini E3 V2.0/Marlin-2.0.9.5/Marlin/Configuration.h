aa55056a5010c20cf551a59084fe1072f91c6a06
RygarNelson <danielemoscati96@hotmail.it>
2023-03-19 15:01:21 +0100

    Aggiornamento di Marlin

--- BTT SKR Mini E3 V2.0/Marlin-2.0.9.5/Marlin/Configuration.h
+++ BTT SKR Mini E3 V2.0/Marlin-2.0.9.5/Marlin/Configuration.h
@@ -1589,8 +1589,8 @@
 
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -26 //Original: -26
-#define Y_MIN_POS -5 //Original: -6
+#define X_MIN_POS -20 //Original: -26
+#define Y_MIN_POS 0 //Original: -6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
