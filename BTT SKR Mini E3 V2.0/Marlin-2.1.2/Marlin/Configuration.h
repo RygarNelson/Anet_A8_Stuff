6d5f59f5c39c071d80dbf80fd26bf1c11f8362ea
RygarNelson <danielemoscati96@hotmail.it>
2023-04-09 23:05:56 +0200

    Abilitato Mesh Bed Levelling

--- BTT SKR Mini E3 V2.0/Marlin-2.1.2/Marlin/Configuration.h
+++ BTT SKR Mini E3 V2.0/Marlin-2.1.2/Marlin/Configuration.h
@@ -1649,7 +1649,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -2025,7 +2025,7 @@
  * Add a bed leveling sub-menu for ABL or MBL.
  * Include a guided procedure if manual probing is enabled.
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
